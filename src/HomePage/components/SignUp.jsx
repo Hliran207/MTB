4f4e7161352e8724015d85c7b532926fbd2d6f10
Hliran207 <liranho@ac.sce.ac.il>
2024-02-27 17:40:02 +0200

    add sign up form

--- src/HomePage/components/SignUp.jsx
+++ src/HomePage/components/SignUp.jsx
@@ -1,4 +1,24 @@
 
 export default function SignUp(){
-    return <h1>Sign Up</h1>
+
+    const signUpUser = (e) =>{
+        e.preventDefault()
+    }
+
+    return (
+        <>
+        <h1 className="sign-up-head">SIGN UP</h1>
+         <div className="sign-up-container">
+            <form className ="sign-up-form" onSubmit={signUpUser}>
+                <label className="sign-up-lable">Name</label>
+                <input type="text" placeholder="Enter Name..." />
+                <label className="sign-up-lable">Email</label>
+                <input type="Email" placeholder="Enter Email..." />
+                <label className="sign-up-lable">Password</label>
+                <input type="password" placeholder="Enter Password..." />
+                <button className="sign-up-button" type="submit">Submit</button>
+            </form>
+            </div>
+        </>
+    )
 }
