92dd0ccb4e04bbf4ea45060abb265a17442b8332
Hliran207 <liranho@ac.sce.ac.il>
2024-02-28 10:17:21 +0200

    create log in and change pic

--- src/HomePage/components/SignUp.jsx
+++ src/HomePage/components/SignUp.jsx
@@ -7,16 +7,22 @@ export default function SignUp(){
 
     return (
         <>
-        <h1 className="sign-up-head">SIGN UP</h1>
-         <div className="sign-up-container">
-            <form className ="sign-up-form" onSubmit={signUpUser}>
-                <label className="sign-up-lable">Name</label>
-                <input type="text" placeholder="Enter Name..." />
-                <label className="sign-up-lable">Email</label>
-                <input type="Email" placeholder="Enter Email..." />
-                <label className="sign-up-lable">Password</label>
-                <input type="password" placeholder="Enter Password..." />
-                <button className="sign-up-button" type="submit">Submit</button>
+        <h1 className="sign-log-head">SIGN UP</h1>
+         <div className="sign-loh-container">
+            <form className ="sign-log-form" onSubmit={signUpUser}>
+                <div className="container-input">
+                    <label className="sign-log-lable">Name</label>
+                    <input type="text" placeholder="Enter Name..." className="sign-log-lable" />
+                </div>
+                <div className="container-input">
+                    <label className="sign-log-lable">Email</label>
+                    <input type="Email" placeholder="Enter Email..."className="sign-log-lable" />
+                </div>
+                <div className="container-input">
+                    <label className="sign-log-lable">Password</label>
+                    <input type="password" placeholder="Enter Password..."className="sign-log-lable"/>
+                </div>
+                <button className="sign-log-button" type="submit">Submit</button>
             </form>
             </div>
         </>
