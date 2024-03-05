b6326cc69b8c9309a1cdff08ea85be60aab70ab8
unknown <sapirsh5@ac.sce.ac.il>
2024-03-05 10:51:23 +0200

    add_to_Lpage

--- src/HomePage/components/hero.jsx
+++ src/HomePage/components/hero.jsx
@@ -1,4 +1,22 @@
+import { MdAir } from "react-icons/md";
+import { FaHandsHoldingChild } from "react-icons/fa6";
+import Features from "./Features";
+function Hero() {
+  return (
+    <>
+      <div className="hero">
+        <h1 className="primary-heading">
+          <FaHandsHoldingChild />
+          רגע לנשום
+        </h1>
 
-export default function Hero(){
-    return <h1>Home</h1>
+        <p className="primary-text">
+          המענה הראשוני לטיפול בלחץ נפשי בקרב ילדים
+        </p>
+        <Features></Features>
+      </div>
+    </>
+  );
 }
+
+export default Hero;
