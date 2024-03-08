3d1aca349bf995bc543b5644b60478b92b2cfcd6
lucasker98 <lucaske@ac.sce.ac.il>
2024-03-08 11:51:49 +0200

    FINAL working breath page, working unit test, working styels to the breath tab

--- client/src/ChildPage/components/TabChildPage.jsx
+++ client/src/ChildPage/components/TabChildPage.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable react/prop-types */
 // Tab.jsx
 import BreathTab from '../Breath/BreathTab';
+import './TabChildPage.css'
 function TabChildPage({ tab }) {
   return (
-    <button className="tab-panel">
-    {tab.id === 'breath' && <BreathTab />}
-    <div className="tab-title">{tab.name}</div>
-  </button>
+    <button className={`tab-panel ${tab.id === 'breath' ? 'breath' : ''}`}>
+      {tab.id === 'breath' && <BreathTab />}
+      <div className="tab-title">{tab.name}</div>
+    </button>
   );
 }
 
